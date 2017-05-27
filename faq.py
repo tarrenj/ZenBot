b12a179b5ff66c44118e0bf24b8942608b954585
tarrenj <jake.tarren@gmail.com>
2017-05-27 21:42:05 +0000

    Updates FAQ dict to point at new repo

--- faq.py
+++ faq.py
@@ -17,7 +17,7 @@ faqs = {
    "exchange": "Many people here recommend using Bittrex: https://bittrex.com/",
    "translations": "We have several people working on translations, checkout #documentation if you want ot help out!",
    "secure node setup": "Blockops has started writing a guide on this:  https://blockoperations.com/build-zencash-secure-node-part-1-prepare-vps/",
-   "about": "This bot was written by tarrenj, and is generously hosted on a VPS provided by our lord and savior joshuayabut.  The source is at https://github.com/tarrenj/ZenFAQBot.  Pull requests are welcome!",
+   "about": "This bot was written by tarrenj, and is generously hosted on a VPS provided by our lord and savior joshuayabut.  The source is at https://github.com/tarrenj/ZenBot.  Pull requests are welcome!",
    "snapshot": "Anyone who had any zClassic at block 111,000 will be given 1 ZenCash for each zClassic they had.  You are free to move your zClassic without effecting your ZenCash",
    "why did you kill yourself?": "That, detective, is the right question.",
    "is there a problem with the three laws?": "The three laws are perfect."
