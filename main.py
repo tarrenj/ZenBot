a0971f47869a92a8f601650745e10c1f5124ef80
tarrenj <jake.tarren@gmail.com>
2017-05-28 23:53:29 +0000

    faq is working!  give works when you don't use an "@" in the post...

--- main.py
+++ main.py
@@ -18,12 +18,10 @@ def getAnswer(question):
     Param question(string): the command
     Return answer(string): the desiered response
     """
-    # Remove any trailing and leading whitespace
-    question = question.strip()
-    if question.startswith('faq'):
+    if question.startswith("faq"):
         try:
-            print 'cmd: ' + cmd.split(' ', 1)
-            rsp = faq.getRsp(str(cmd.split(' ', 1)[1]))
+            option = question.split(' ', 1)[1]
+            rsp = faq.getRsp(option)
             return rsp
         except:
             return '(Probably) Invalid FAQ entry.  See: https://github.com/tarrenj/ZenBot/blob/master/faq.py'
@@ -48,13 +46,12 @@ def main():
                     # Get an easy name for the post (minus the bang)
                     cmd = post['text'][1:]
                     # Who are we flagging in the response?  (If it's a DM, fireAway wont bother flagging)
+                    target = utils.getUserName(post['user'])
                     if cmd.startswith('give'):
                         # Make sure we're giving to a valid user
                         target = utils.getUserName(cmd.split(' ', 1)[1].split(' ', 1)[0])
                         # Get everything after the given name
-                        cmd = cmd.split(' ', 1)[1].split(' ', 1)[1]
-                    else:
-                        target = utils.getUserName(post['user'])
+                        cmd = cmd.split(' ', 1)[1].split(' ', 1)[1].strip()
                     resp = getAnswer(cmd)
                     utils.fireAway(resp, target, post['channel'])
 
