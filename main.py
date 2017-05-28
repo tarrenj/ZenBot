95d208d69133c4d9035a5b0f87036884ef88ee92
tarrenj <jake@zencash.io>
2017-05-28 02:59:30 -0400

    Adds faq command processing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #/usr/bin/env python
 
 import utils
+import faq
 import os
 from slackclient import SlackClient
 import responses # All responses go in dictionary format in this file, this is to keep it easy for people to submit PRs with updates/corrections.
@@ -29,6 +30,12 @@ def main():
                 if 'text' in post and post['text'].startswith('!'):
                     print 'Ahh, I\'m triggered!'
                     print post
+                    # Get an easy name for the post (minus the bang)
+                    cmd = post['text'][1:]
+                    # Start checking for things to do
+                    if cmd.startswith('faq'):
+                        resp = faq.getResp(cmd.split(' ', 1)[1]
+                        print resp
 
 # Boiler plate....
 if __name__ == "__main__":
