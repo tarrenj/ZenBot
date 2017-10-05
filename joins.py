1b2b4d0b85abd7ebf1bafc6de5becf1843e33a74
Jake Tarren <jake@zensystem.io>
2017-10-05 16:14:14 -0400

    Adds a stripped down verson that only welcomes new users

--- joins.py
+++ joins.py
@@ -9,11 +9,28 @@ def newJoin(user, channel):
     Params: user(string), channel(string)
     Returns: none
     """
+
+    message = """
+Welcome to the official Slack for ZenCash!
+
+
+The official links are:
+https://github.com/ZenCashOfficial/
+https://zencashofficial.io/
+
+NOTICE:
+Due to the recent plague that is SlackBot spamming with `/remind`, please do not click any links sent to you in a DM from slackbot.
+
+Additionally, please copy the message to #spam so the Admins can ban the user.
+
+Please remember to be civil, and have a great day!
+"""
+
+
     # General
     if channel == 'C4QGQ8SEM':
-         message = "Welcome to the official Slack for ZenCash!\n\nThe official links are:\nhttps://github.com/ZenCashOfficial/\nhttps://zencashofficial.io/\n\nWe are not affiliated with:\nhttps://github.com/zencashio/\nhttps://zencash.io/\n\nPlease remember to be civil, and have a great day!"
          return message
 
     # Bottesting
     if channel == "C5JCER3NG":
-        pass
+        return message
---
+++ welcomeOnly.py
@@ -0,0 +1,43 @@
+#/usr/bin/env python
+
+import utils
+import joins
+import os
+from slackclient import SlackClient
+
+# Get the things!
+APIToken = os.environ["SLACK_BOT_TOKEN"]
+BotName = 'zenbot'
+READ_WEBSOCKET_DELAY = 1
+SC = SlackClient(APIToken)
+BotToken = ''
+
+
+def main():
+    """
+    Where all the magic happens
+    Params: None
+    Returns: None
+    """
+    # Start off by getting the token.
+    global BotToken
+    BotToken = utils.getUserToken(BotName)
+    
+    if SC.rtm_connect():
+        while True:
+            # This triggers me
+            for post in SC.rtm_read():
+                # Catch all errrors...
+                try:
+                    # Check for any user join
+                    if post['type'] == 'member_joined_channel':
+                        resp = joins.newJoin(utils.getUserName(post['user']), post['channel'])
+                        utils.fireAway(resp, post['user'])
+                    except:
+                        # Should eventually send error message to @smrtz
+                        e = sys.exec_info()[0]
+                        print e
+
+# Boiler plate....
+if __name__ == "__main__":
+    main()
